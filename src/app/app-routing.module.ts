9bcf846218186afebae7a90cd00e63cd4717b197
Thomas Schubnel <schubnel31@gmail.com>
2022-10-12 16:17:01 +0200

    Add NFC page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,11 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'nfc',
+    loadChildren: () => import('./nfc/nfc.module').then( m => m.NfcPageModule)
+  },
+
 ];
 
 @NgModule({
